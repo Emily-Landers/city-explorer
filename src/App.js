0bd07f96cbc832b99772a46b6271a407f973e147
Emily Landers <emily.j.landers@gmail.com>
2021-12-07 10:03:52 -0500

    progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './Header.js';
+// import Header from './Header.js';
+import axios from 'axios';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+require('dotenv').config();
 
 class App extends Component {
-render (){
-  return(
-      <Header />
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      queryCity: '',
+      locationObject: {},
+      error: false
+    }
+  }
+
+  getLocation = async() => {
+    try {
+      let result = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.password}&q=${this.state.queryCity}&format=json`);
+        console.log(result.data[0]);
+        this.setState({ locationObject: result.data[0]})
+    } catch (error) {
+      console.error(error);
+      console.log('there was an error');
+      this.setState({ error: true })
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ queryCity: e.target.value }, this.getLocation);
+  }
+
+  render() {
+    return (
+      <div>
+              <Form onSubmit={this.handleSubmit}>
+            <Form.Group>
+              <Form.Control type="text" placeholder="city name" name="city" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+             Explore!
+            </Button>
+          </Form>
+        {this.state.locationObject.display_name? <p>{this.state.locationObject.display_name}</p> : <p>Search for a city to explore</p>}
+        {this.state.error && <p>There was an error with your request</p>}
+      </div>
     )
   }
 }
---
+++ src/FormExplore.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+export default class FormExplore extends Component {
+    render() {
+
+        return (
+      
+        )
+    }
+}
