517a3c9efc06dbbc80cb23c15cd4f4fe8d6b6566
Emily Landers <emily.j.landers@gmail.com>
2021-12-07 22:32:04 -0500

    finishes lab 6

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import axios from 'axios';
 import FormExplore from './FormExplore.js';
 import CityInfo from './CityInfo.js';
+import ErrorMsg from './ErrorMsg.js';
 
 
 export default class App extends Component {
@@ -12,7 +13,7 @@ export default class App extends Component {
     super(props);
     this.state = {
       locationObject: {},
-      error: false
+      showError: false
     };
   }
 
@@ -23,24 +24,28 @@ export default class App extends Component {
       let result = await axios.get(url);
         this.setState({ locationObject: result.data[0]}, this.mapImage)
     } catch (error) {
-      console.error(error);
-      console.log('Error');
-      this.setState({ error: true })
+        this.openError()
     }
   }
 mapImage = () =>{
   let url =`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.state.locationObject.lat},${this.state.locationObject.lon}&zoom=13`;
   this.setState({locationObject: { ...this.state.locationObject, map: url }})
 }
-
+openError = () => {
+  this.setState({showError: true});
+}
+  
+  closeError = () => {
+    this.setState({showError: false});
+  }
 
 
   render() {
     return (
       <div>
-        {/* {this.result.data[0]} */}
         <FormExplore getLocation={this.getLocation}/>
         <CityInfo locationObject={this.state.locationObject} />
+        <ErrorMsg showError={this.state.showError} closeError={this.closeError}/>
       </div>
     )
   }
